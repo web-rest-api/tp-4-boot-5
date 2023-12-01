165ae12675c7bf024de67b835ca5f7f7c6457ca9
lostmart <martinpedraza1979@hmail.com>
2023-12-01 14:34:36 +0100

    new games

---
+++ script.js
@@ -0,0 +1,17 @@
+/*  top games  */
+const gameList = [
+	{
+		title: "Hollow knight",
+		year: 2017,
+		imgUrl:
+			"https://gaming-cdn.com/images/products/2198/616x353/hollow-knight-pc-mac-game-steam-cover.jpg?v=1694101703",
+	},
+	{
+		title: "FC24",
+		year: 2024,
+		imgUrl:
+			"https://wp.inews.co.uk/wp-content/uploads/2023/08/EA-Sports-FC-24-scaled-1.jpeg?resize=640,360&strip=all&quality=90",
+	},
+]
+
+console.log("test 2")
